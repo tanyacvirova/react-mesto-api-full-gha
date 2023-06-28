4fb7d5cc0bd424490961847d4eb00340532e65a5
Tanya Tsvirova <ta.cvirova@yandex.ru>
2023-06-28 17:05:42 +0300

    refactor: добавлен cors

---
+++ backend/middlewares/cors.js
@@ -0,0 +1,25 @@
+const allowedCors = [
+  'https://mestoproject.tanyacvirova.nomoreparties.sbs',
+  'http://mestoproject.tanyacvirova.nomoreparties.sbs',
+  'localhost:3000',
+];
+
+// eslint-disable-next-line consistent-return
+module.exports = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  next();
+};
